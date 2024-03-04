e2a41acc0f9860af4ce09e222661a3a0d7dd64e1
Fernando <fh1273509@gmail.com>
2024-03-04 16:22:40 -0300

    termino con number, date y cvv..

--- main.js
+++ main.js
@@ -22,6 +22,27 @@ inputCard.addEventListener("keydown", (e) => {
   handleInput(maskNumber, e.key, cardNumber);
   inputCard.value = cardNumber.join("");
 });
+
+inputDate.addEventListener("keydown", (e) => {
+  if (e.key == "Tab") {
+    return;
+  }
+
+  e.preventDefault();
+  handleInput(maskDate, e.key, cardDate);
+  inputDate.value = cardDate.join("");
+});
+
+inputCvv.addEventListener("keydown", (e) => {
+  if (e.key == "Tab") {
+    return;
+  }
+
+  e.preventDefault();
+  handleInput(maskCvv, e.key, cardCvv);
+  inputCvv.value = cardCvv.join("");
+});
+
 function handleInput(mask, key, arr) {
   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   if (key === "Backspace" && arr.length > 0) {
